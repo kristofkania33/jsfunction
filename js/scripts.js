cd817f2dd0144c272209217478e1ff91e34ae5d5
kristofkania33 <zyhkana25@wp.pl>
2017-05-30 21:00:03 +0200

    function

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+// scripts.js
+var a = prompt ("Enter  triangle's side length: ");
+var h = prompt ("Enter  triangle's height length: ");
+
+
+function getTriangleArea(a, h) {
+	if 
+		(a < 0 || a == 0 || h < 0 || h == 0)
+	{return "Data is incorrect!!!";}
+	else if
+		(a > 0 && h > 0)
+	{return "Triangle field where base a is equal: " + a + " and height h is equal: " + h + " is equal: " + a*h/2;}
+}
+    
+	var functionValue = getTriangleArea(a, h);
+	console.log(functionValue);
+
+	var function1Value = getTriangleArea(8, 9);
+	console.log(function1Value);
+
+	var function2Value = getTriangleArea(3, 4);
+	console.log(function2Value);
+
+	var function3Value = getTriangleArea(12, 4);
+	console.log(function3Value);
+
