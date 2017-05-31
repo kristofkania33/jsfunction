edac81aae8ce42bc12a014f5bdce987ad4024389
kristofkania33 <zyhkana25@wp.pl>
2017-05-31 12:43:55 +0200

    new structure

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
 // scripts.js
 
 function getTriangleArea(a, h) {
-	if(a > 0 && h > 0) {
+    if(a > 0 && h > 0) {
 		return "Triangle field where base a is equal: " + a + " and height h is equal: " + h + " is equal: " + a*h/2;
     } 
-	else if (a <= 0 || h <= 0) {
+    else if (a <= 0 || h <= 0) {
     	return "Data is incorrect";
 	}
 }
     
 	
-	var TriangleArea1 = getTriangleArea(8, 9);
-	console.log(TriangleArea1);
+var TriangleArea1 = getTriangleArea(8, 9);
+console.log(TriangleArea1);
 
-	var TriangleArea2 = getTriangleArea(3, 4);
-	console.log(TriangleArea2);
+var TriangleArea2 = getTriangleArea(3, 4);
+console.log(TriangleArea2);
 
-	var TriangleArea3 = getTriangleArea(12, 4);
-	console.log(TriangleArea3);
+var TriangleArea3 = getTriangleArea(12, 4);
+console.log(TriangleArea3);
 
